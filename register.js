661622de8afc443545e773f55e6578e1cb877afb
ZaiManzano <gianmanzano123@gmail.com>
2024-03-05 16:03:07 +0800

    add html and js files

---
+++ register.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+console.error("Enggggg error!");
+console.warn("Warning Hazard!");
+
+console.log(typeof["123"]);
+
+
+let name = "zai",
+lastname = "Manzano",
+ age = 19,
+IsStudent = true,
+ hobbies = ["Playing volleyball"];
+console.log(name, age, IsStudent, hobbies);
+console.table(hobbies);
+console.log("My name is " + name + "" + lastname);
+console.log(`My name is ${name} ${lastname}`);
